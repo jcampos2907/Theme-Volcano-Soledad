c778992e63bd214256564c545591efd63c6b0967
Juan Campos <nacho.campos29@gmail.com>
2023-05-15 15:52:46 -0600

    pruebas

--- assets/multi-step-pruebas.js
+++ assets/multi-step-pruebas.js
@@ -374,13 +374,12 @@ function validateOrder() {
   const input = x[currentTab].querySelector(".preventa");
   if (!input) return valid
   const options = {
-    method: 'GET',
     url: `https://8cfb-186-96-89-74.ngrok-free.app/check_code/${input.value}`,
     headers: {
       'Content-Type': 'application/json',
     }
   };
-  axios.request(options).then(function (response) {
+  axios.get(options).then(function (response) {
     console.log(response)
   })
   let container = input.closest('p')
