e326052dbfc11f80b0f7d26844fa883b1f07a93e
Juan Campos <nacho.campos29@gmail.com>
2023-05-16 20:49:00 -0600

    pruebas

--- assets/form-inscripcion2.js
+++ assets/form-inscripcion2.js
@@ -678,64 +678,67 @@ function PopulateCountries() {
         });
       })
       const telefonoSelects = document.querySelectorAll('select.codigo_telefono');
+      getPhoneCode().then(examples => {
+        telefonoSelects.forEach(select => {
+          const preferredGroup = document.createElement('optgroup');
+          preferredGroup.label = 'Países preferidos';
+          select.appendChild(preferredGroup);
+          preferredCountryCodes.forEach(countryCode => {
+            const preferredCountry = countries.find(country => country.cca2 === countryCode);
+            if (preferredCountry) {
+              const option = document.createElement('option');
+              if (countryCode == 'CR') option.setAttribute('selected', '')
+              option.textContent = preferredCountry.translations.spa.common || preferredCountry.name.common;
+              option.setAttribute('data-style', `background-image: url('${preferredCountry.flags.png}')`)
+              const code = document.createElement('span');
+              const phoneCode = libphonenumber.getExampleNumber(preferredCountry.cca2, value)
+              code.textContent = phoneCode
+              option.value = JSON.stringify({ code: code.textContent, countrycode: preferredCountry.cca2, image: preferredCountry.flags.png });
+              preferredGroup.appendChild(option);
+              // getPhoneCode(preferredCountry.cca2).then(phoneCode => {
+              //   code.textContent = phoneCode
+              //   option.value = JSON.stringify({ code: code.textContent, countrycode: preferredCountry.cca2, image: preferredCountry.flags.png });
+              //   preferredGroup.appendChild(option);
+              // })
 
-      let examples = {};
+              // console.log(libphonenumber.getExampleNumber("CR", value))
+              // code.textContent = preferredCountry.idd.root;
+              // if(preferredCountry.idd.suffixes.length == 1)code.textContent = `${preferredCountry.idd.root}${preferredCountry.idd.suffixes}`;
+            }
+          });
+          // Create a separator option between the preferred and non-preferred countries
+          const separator = document.createElement('option');
+          separator.disabled = true;
+          separator.textContent = '---------------------------';
+          select.appendChild(separator);
 
-      getPhoneCode().then(response => {
-        examples = response;
+          // Create a group for the rest of the countries
+          const restGroup = document.createElement('optgroup');
+          restGroup.label = 'Todos los países';
+          select.appendChild(restGroup);
+          // Create and append options for the rest of the countries
+          countries.forEach(country => {
+            if (!preferredCountryCodes.includes(country.cca2)) {
+              const option = document.createElement('option');
+              option.textContent = country.translations.spa.common || country.name.common;
+              option.setAttribute('data-style', `background-image: url('${country.flags.png}')`)
+              const code = document.createElement('span');
+              const phoneCode = libphonenumber.getExampleNumber(country.cca2, value)
+              code.textContent = phoneCode
+              option.value = JSON.stringify({ code: code.textContent, countrycode: country.cca2, image: country.flags.png });
+              preferredGroup.appendChild(option);
+              // getPhoneCode(country.cca2).then(phoneCode => {
+              //   code.textContent = phoneCode
+              //   option.value = JSON.stringify({ code: code.textContent, countrycode: country.cca2, image: country.flags.png });
+              //   restGroup.appendChild(option);
+              // })
+              // code.textContent = country.idd.root; 
+              // if(country.idd.suffixes && country.idd.suffixes.length == 1)code.textContent = `${country.idd.root}${country.idd.suffixes}`;
+            }
+          });
+        });
       });
 
-      console.log(examples)
-      telefonoSelects.forEach(select => {
-        const preferredGroup = document.createElement('optgroup');
-        preferredGroup.label = 'Países preferidos';
-        select.appendChild(preferredGroup);
-        preferredCountryCodes.forEach(countryCode => {
-          const preferredCountry = countries.find(country => country.cca2 === countryCode);
-          if (preferredCountry) {
-            const option = document.createElement('option');
-            if (countryCode == 'CR') option.setAttribute('selected', '')
-            option.textContent = preferredCountry.translations.spa.common || preferredCountry.name.common;
-            option.setAttribute('data-style', `background-image: url('${preferredCountry.flags.png}')`)
-            const code = document.createElement('span');
-            // getPhoneCode(preferredCountry.cca2).then(phoneCode => {
-            //   code.textContent = phoneCode
-            //   option.value = JSON.stringify({ code: code.textContent, countrycode: preferredCountry.cca2, image: preferredCountry.flags.png });
-            //   preferredGroup.appendChild(option);
-            // })
-
-            // console.log(libphonenumber.getExampleNumber("CR", value))
-            // code.textContent = preferredCountry.idd.root;
-            // if(preferredCountry.idd.suffixes.length == 1)code.textContent = `${preferredCountry.idd.root}${preferredCountry.idd.suffixes}`;
-          }
-        });
-        // Create a separator option between the preferred and non-preferred countries
-        const separator = document.createElement('option');
-        separator.disabled = true;
-        separator.textContent = '---------------------------';
-        select.appendChild(separator);
-
-        // Create a group for the rest of the countries
-        const restGroup = document.createElement('optgroup');
-        restGroup.label = 'Todos los países';
-        select.appendChild(restGroup);
-        // Create and append options for the rest of the countries
-        countries.forEach(country => {
-          if (!preferredCountryCodes.includes(country.cca2)) {
-            const option = document.createElement('option');
-            option.textContent = country.translations.spa.common || country.name.common;
-            option.setAttribute('data-style', `background-image: url('${country.flags.png}')`)
-            const code = document.createElement('span');
-            // getPhoneCode(country.cca2).then(phoneCode => {
-            //   code.textContent = phoneCode
-            //   option.value = JSON.stringify({ code: code.textContent, countrycode: country.cca2, image: country.flags.png });
-            //   restGroup.appendChild(option);
-            // })
-            // code.textContent = country.idd.root; 
-            // if(country.idd.suffixes && country.idd.suffixes.length == 1)code.textContent = `${country.idd.root}${country.idd.suffixes}`;
-          }
-        });
-      });
     })
     .catch(error => {
       console.error('Error al obtener datos de los países:', error);
